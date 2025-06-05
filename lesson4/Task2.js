e52770e8b4b8fcb9651974a2111715de2de378cc
Ivan Ksenk <ivan.ksenk@gmail.com>
2025-06-05 15:11:34 +0500

    JS_lesson_4_iKosenko@

--- lesson4/Task2.js
+++ lesson4/Task2.js
@@ -1,4 +1,20 @@
 const obj = { prop1: 1, prop2: 2, prop3: true, prop4: 'test', prop5: 10 }
-let resultObj = {}
-...
+
+
+function evenProps(obj) {
+    if (obj === null || typeof (obj) !== 'object') {
+        return console.log('Invalid object');
+    }
+    const result = {};
+
+    for (key in obj){
+        if(obj[key] %2 === 0){
+            result[key] = obj[key]
+        }
+    }
+    return result;
+}
+
+const resultObj = evenProps(obj);
+
 console.log(resultObj); // { prop2: 2, prop5: 10 }
